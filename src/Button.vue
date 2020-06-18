69b3172e919aa35f445d2baf4468587db3794aee
Freedy <21914103@zju.edu.cn>
2020-06-18 13:32:54 +0800

    单文件组件创建完成

--- src/Button.vue
+++ src/Button.vue
@@ -1,19 +1,13 @@
 
 <template>
-  <div id="app">
-    <button class="g-button">按钮</button>
-  </div>
+  <button class="g-button">按钮</button>
 </template>
 
 <script>
+export default {};
 </script>
 
 <style lang="scss">
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 :root {
   --button-height: 32px;
   --font-size: 14px;
@@ -24,12 +18,6 @@
   --border-color: #999;
   --border-color-hover: #666;
 }
-#app {
-  margin: 20px;
-}
-body {
-  font-size: var(--font-size);
-}
 .g-button {
   font-size: var(--font-size);
   height: var(--button-height);
@@ -37,14 +25,14 @@ body {
   border-radius: var(--button-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-}
-.g-button:hover {
-  border-color: var(--border-color-hover);
-}
-.g-button:active {
-  background-color: var(--button-active-bg);
-}
-.g-button:focus {
-  outline: none;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Button from './Button.vue'
 
+Vue.component('g-button', Button)
+
 Vue.config.productionTip = false
 
 new Vue({
