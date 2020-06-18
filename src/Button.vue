86db12ea3d204d71d18c493bf875ef3f3b28ad89
Freedy <21914103@zju.edu.cn>
2020-06-18 13:07:53 +0800

    完成按钮基本样式

---
+++ src/Button.vue
@@ -0,0 +1,50 @@
+
+<template>
+  <div id="app">
+    <button class="g-button">按钮</button>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+:root {
+  --button-height: 32px;
+  --font-size: 14px;
+  --button-bg: white;
+  --button-active-bg: #eee;
+  --button-radius: 4px;
+  --color: #333;
+  --border-color: #999;
+  --border-color-hover: #666;
+}
+#app {
+  margin: 20px;
+}
+body {
+  font-size: var(--font-size);
+}
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--button-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+}
+.g-button:hover {
+  border-color: var(--border-color-hover);
+}
+.g-button:active {
+  background-color: var(--button-active-bg);
+}
+.g-button:focus {
+  outline: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Button from './Button.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Button),
 }).$mount('#app')
